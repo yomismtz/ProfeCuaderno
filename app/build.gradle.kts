947c27e9332147a1e8b0901abe85ed22ad290f4a
yomismtz <yomismtz@gmail.com>
2026-09-09 13:32:01 -0600

    Target Android 16 and bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.profecuaderno.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.profecuaderno.app"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        targetSdk = 36
+        versionCode = 6
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }

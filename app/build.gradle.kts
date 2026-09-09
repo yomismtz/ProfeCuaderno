b34635f4422cc1a0f27c011b110d34b0dc5e7fa4
yomismtz <yomismtz@gmail.com>
2026-09-09 17:00:16 -0600

    Bump version for picker and attendance fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.profecuaderno.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7.0"
+        versionCode = 9
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }

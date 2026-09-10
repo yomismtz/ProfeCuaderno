68ceed89c9ae9efb6efc158976a4b9e63c2ccab1
yomismtz <yomismtz@gmail.com>
2026-09-09 20:22:13 -0600

    Bump version after stability audit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.profecuaderno.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.7.2"
+        versionCode = 11
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
